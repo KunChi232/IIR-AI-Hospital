bd386985032d48b75eaa04600c2f7d99529d6f51
KunChi <kuo23201199@gmail.com>
2020-03-16 16:23:19 +0800

    init project

---
+++ app/src/main/java/com/example/iir_ai_hospital/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.iir_ai_hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*
+         * Hide navigate bar
+         * https://stackoverflow.com/questions/21724420/how-to-hide-navigation-bar-permanently-in-android-activity
+         */
+        final int flags = View.SYSTEM_UI_LAYOUT_FLAGS
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        this.getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        int PERMISSIONS_ALL = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+        };
+        if(hasPermission(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_ALL);
+        }
+    }
+
+    private boolean hasPermission(Context context, String... permissions) {
+        if(context != null && permissions != null) {
+            for(String permission : permissions) {
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+             }
+        }
+        return true;
+    }
+}
